74693808e87c9cdf3ff3997daaab37ad19255b42
vagrant <vagrant@localhost.localdomain>
2020-05-02 08:58:47 +0000

    [start]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  
   devise_for :users
   root 'home#top'
-  get 'home/about'
+  get 'home/about', to: 'home#about'
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+  
+end
