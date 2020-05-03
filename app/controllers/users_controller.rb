6f9e54b87188fc8ab83cf167436337dea11f3998
vagrant <vagrant@localhost.localdomain>
2020-05-03 06:25:09 +0000

    [finish-test]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,14 +17,14 @@ class UsersController < ApplicationController
   def edit
   	@user = User.find(params[:id])
     if @user != current_user
-        redirect_to user_path(current_user)
+        redirect_to user_path(current_user.id)
     end
   end
 
   def update
   	@user = User.find(params[:id])
   	if @user.update(user_params)
-  		redirect_to users_path(@user.id), notice: "successfully updated user!"
+  		redirect_to user_path(@user.id), notice: "successfully updated user!"
   	else
   		render "users/edit"
   	end
